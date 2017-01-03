e948c95b67fd01bc1cd180de6ac15d68cfc9af20
Martino Facchin <m.facchin@arduino.cc>
2017-01-03 14:30:53 +0100

    Refactor first configuration example

--- examples/FirstConfiguration/FirstConfiguration.ino
+++ examples/FirstConfiguration/FirstConfiguration.ino
@@ -1,64 +1,116 @@
-#include "SigFox.h"    // Atmel SIGFOX shield library 
+/*
+  SigFox Getting started
+
+  This sketch demonstrates the usage of MKR3000 SigFox module.
+  Since the board is designed with lowe power in mind, it depends directly on ArduinoLowPower library
+
+  This example code is in the public domain.
+*/
+
+#include "SigFox.h"
 #include "ArduinoLowPower.h"
-
-//#define Serial Serial1
-
-#define lpb 60
-char pbuff[lpb];
-
-unsigned long timer;          // timer for sleeping
-unsigned int count = 0;       // samples counter
-
-boolean ishere;
 
 void setup() {
   Serial.begin(115200);
   while (!Serial) {};
 
+  // Uncomment this line and comment begin() if you are working with a custom board
+  //if (!SigFox.begin(SPI1, 30, 31, 33, 28, LED_BUILTIN)) {
   if (!SigFox.begin()) {
     Serial.println("Shield error or not present!");
     return;
   }
-  delay(100);
-  unsigned char *buffer;
-  buffer = SigFox.getSigVersion();            // get version
-  unsigned char ver[2] = {buffer[0], buffer[1]};
-  buffer = SigFox.getID();                    // get ID
-  unsigned char ID[4] = {buffer[0], buffer[1], buffer[2], buffer[3]};
-  unsigned char *PAC = SigFox.getPAC();       // get PAC
-  // display informations
+  // Enable debug led and disable automatic deep sleep
+  // Comment this line when shipping your project :)
+  SigFox.debug(true);
+
+  String version = SigFox.getSigVersion();
+  String ID = SigFox.getID();
+  String PAC = SigFox.getPAC();
+
+  // Display module informations
   Serial.println("MKR3000 Sigfox first configuration");
-  snprintf(pbuff, lpb, "SigFox FW version -V%d.%d\r\n", ver[0], ver[1]);
-  Serial.println(pbuff);
-  snprintf(pbuff, lpb, "ID  = %02X%02X%02X%02X\r\n", ID[3], ID[2], ID[1], ID[0]);
-  Serial.println(pbuff);
-  Serial.print("PAC = ");
-  int i; for (i = 0; i < 8; i++) {
-    snprintf(pbuff, lpb, "%02X", PAC[i]);
-    Serial.print(pbuff);
-  }
-  Serial.println("");
-  Serial.println("");
-  Serial.println("Click here to register the board on SigFox network");
+  Serial.println("SigFox FW version " + version);
+  Serial.println("ID  = " + ID);
+  Serial.println("PAC = " + PAC);
 
-  SigFox.setEUMode();
+  Serial.println("");
 
-  SigFox.end();               // switch off SIGFOX module
-  
-  sendString("Tanti auguri");
+#if ARDUINO >= 10801
+  Serial.println("Click <a href=\"https://backend.sigfox.com/new?pac=" + PAC + "?id=" + ID +  "\">here</a> to register the board on SigFox network");
+#else
+  Serial.println("Register your board on https://backend.sigfox.com with provided ID and PAC");
+#endif
+
+  // Set region and transmission mode
+  // if TX - only mode is selected, function receive() will not be available
+  // Possible regions are US and EU
+  SigFox.setMode(EU, TXRX);
+
+  // Send the module to the deepest sleep
+  SigFox.end();
+
+  // Example of message that can be sent
+  //sendString("Hello world!");
+
+  // Example of send and read response
+  sendStringAndGetResponse("Hello world!");
 }
 
 void loop()
 {
+  // Uncomment to sleep for 10minutes
+  // LowPower.sleep(10 * 60 * 1000);
+  // Check if something happened and eventually report back
+  // result = do_the_check();
+  // if (result == true)
+  //  sendString("something")
 }
 
 void sendString(String str) {
+  // Start the module
   SigFox.begin();
+  // Wait at least 30mS after first configuration (100mS before)
+  delay(100);
+  // Clears all pending interrupts
   SigFox.getStatus();
-  int ret = SigFox.sendMessage((unsigned char*)str.c_str(), str.length());  // send buffer to SIGFOX network
-  Serial.println("message sent");
-  Serial.println(SigFox.getStatusSig());
-  Serial.println(SigFox.getStatusAtm());
-  if (ret > 0) Serial.println("No transmission!");
+  delay(1);
+
+  int ret = SigFox.send(str);  // send buffer to SIGFOX network
+  if (ret > 0)
+    Serial.println("No transmission");
+  else
+    Serial.println("Transmission ok");
+
+  Serial.println(SigFox.getStatus(SIGFOX));
+  Serial.println(SigFox.getStatus(ATMEL));
+  SigFox.end();
+}
+
+void sendStringAndGetResponse(String str) {
+  // Start the module
+  SigFox.begin();
+  // Wait at least 30mS after first configuration (100mS before)
+  delay(100);
+  // Clears all pending interrupts
+  SigFox.getStatus();
+  delay(1);
+
+  int ret = SigFox.receive(str);  // send buffer to SIGFOX network
+  if (ret > 0)
+    Serial.println("No transmission");
+  else
+    Serial.println("Transmission ok");
+
+  Serial.println(SigFox.getStatus(SIGFOX));
+  Serial.println(SigFox.getStatus(ATMEL));
+
+  Serial.println("Response from server:");
+  while (SigFox.available()) {
+    Serial.print("0x");
+    Serial.println(SigFox.read(), HEX);
+  }
+  Serial.println();
+
   SigFox.end();
 }
